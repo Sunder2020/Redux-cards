7bbbe236634a31a40eeffd69d9947edf5d24b1c4
SUNDERBONTHU20 <sunderbonthu7@gmail.com>
2024-05-18 11:15:12 +0530

    d

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,45 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux';
-import {add} from '../store/cartSlice'
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { add } from "../store/cartSlice";
 
 const Products = () => {
   const dispatch = useDispatch();
-  const[products,setProducts]=useState([]);
+  const [products, setProducts] = useState([]);
 
-useEffect(()=>{
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const res = await fetch("https://fakestoreapi.com/products");
+      const data = await res.json();
+      console.log(data);
+      setProducts(data);
+    };
+    fetchProduct();
+  }, []);
 
-const fetchProduct = async()=>{
-     const res = await fetch('https://fakestoreapi.com/products');
-     const data = await res.json();
-     console.log(data);
-     setProducts(data);
-}
-fetchProduct();
-},[])
-
-const handleAdd = (product) =>{
-      dispatch(add(product))
-}
+  const handleAdd = (product) => {
+    dispatch(add(product));
+  };
 
   return (
     <>
-    
-    <div className='productsWrapper'>
-      {
-       products.map((product)=>(
-            <div className='card' key={product.id}>
-            <img src={product.image} alt='products'/>
+      <div className="productsWrapper">
+        {products.map((product) => (
+          <div className="card" key={product.id}>
+            <img src={product.image} alt="products" />
             <h4>{product.title}</h4>
             <h4>{product.price}</h4>
-            <button className='btn' onClick={()=>handleAdd(product)}>ADD TO CART</button>
-
-            </div>
-       ))
-      }
-    </div>
-   
+            <button className="btn" onClick={() => handleAdd(product)}>
+              ADD TO CART
+            </button>
+          </div>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
